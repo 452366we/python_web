527342a1b238d5b3d1261caeb5c907a9a031be8e
terna <ts@tedu.cn>
2019-10-25 17:39:08 +0800

    'news接口完成'

--- python_project/mfresh_project/mfresh_api/news/views.py
+++ python_project/mfresh_project/mfresh_api/news/views.py
@@ -2,17 +2,40 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import JsonResponse
+import math
+from .models import MfNews
 
 def newsList(req):
+    pageNum=req.GET.get('pageNum',1)
+    pageNum=int(pageNum)
+    pageSize=10
+    totalRecord=MfNews.objects.count()
+    pageCount=math.ceil( totalRecord/pageSize )
+    r=MfNews.objects.order_by('-pubtime').values('nid','title','pubtime')
+    start=pageSize*(pageNum-1)
+    end=pageNum*pageSize
+    data=list(r[start:end])
+    print(data,type(data))
     res=JsonResponse({
-        
+        'totalRecord':totalRecord,
+        'pageSize':pageSize,
+        'pageCount':pageCount,
+        'pageNum':pageNum,
+        'data':data
     })
     res['Access-Control-Allow-Origin']='*'
     return res
 
 def newsDetail(req):
-    res=JsonResponse({
-        
-    })
+    nid=req.GET.get('nid')
+    r=MfNews.objects.filter(nid=nid).values()
+    if(len(r)>0):
+        n=r[0]
+        res=JsonResponse({
+            'nid':n['nid'],
+            'title':n['title'],
+            'pubtime':n['pubtime'],
+            'content':n['content']
+        })
     res['Access-Control-Allow-Origin']='*'
     return res
